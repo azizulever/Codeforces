ff4a2ed4aa183ad2064624990e1cb69771d0f5af
Azizul Islam <111518588+azizulislam22@users.noreply.github.com>
2023-02-18 20:08:11 +0600

    Update Twins.cpp

--- Twins.cpp
+++ Twins.cpp
@@ -10,7 +10,7 @@ int main(){
                 cin >> N[i];
                 sum += N[i];
             }
-
+            
             for(i = 0; i < n; i++){
                 for(j = 0; j < n - 1; j++){
                     if(N[j] < N[i]){
