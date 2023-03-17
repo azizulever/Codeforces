f48308352ee348d97b85a511dea9b18e64307d92
Azizul Islam <111518588+azizulislam22@users.noreply.github.com>
2023-03-17 21:04:24 +0600

    update

---
+++ presents.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define int long long
+#define pb push_back
+#define for0 for(int i = 0; i < n; i++)
+#define for1 for(int i = 1; i <= n; i++)
+#define nl endl
+#define tc int t; cin >> t; while(t--)
+using namespace std;
+
+int32_t main() {
+    int n, elem; cin >> n;
+
+     vector<int> v;
+        for0 {
+            cin >> elem;
+            v.pb(elem);
+        }
+
+        for1 {
+            for(int j = 0; j < n; j++) {
+                if(v[j] == i)
+                    cout << j+1 << " ";
+            }
+        }
+    
+    return 0;
+}
