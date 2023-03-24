2a74960d6cd3632338d05c2a2d56dbe3caeddeba
Azizul Islam <111518588+azizulislam22@users.noreply.github.com>
2023-03-24 11:07:26 +0600

    update

---
+++ Gravity Flip.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define int long long
+#define pb push_back
+#define for0 for(int i = 0; i < n; i++)
+#define for1 for(int i = 1; i <= n; i++)
+#define nl endl
+#define tc int t; cin >> t; while(t--)
+using namespace std;
+
+int32_t main() {
+    int n, elem; cin >> n;
+
+    vector<int> v;
+    for0 {
+        cin >> elem;
+        v.pb(elem);
+    }
+
+    sort(v.begin(), v.end());
+    for(auto elem : v) {
+        cout << elem << " ";
+    }
+
+    return 0;
+}
