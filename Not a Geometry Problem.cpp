d8fcf0fbb4d4aa2b639e4a0dcb5cb293043f13fd
Azizul Islam <111518588+azizulislam22@users.noreply.github.com>
2023-04-03 22:53:02 +0600

    update

---
+++ Not a Geometry Problem.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define int long long
+#define pb push_back
+#define vec vector<int>
+#define nl endl
+#define for0 for(int i = 0; i < n; i++)
+#define for1 for(int i = 1; i <= n; i++)
+#define tc int t; cin >> t; while(t--)
+using namespace std;
+
+int32_t main() {
+    cout << 0;
+
+    return 0;
+}
