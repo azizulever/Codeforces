5462185efb470dac082cda36370f743e13d0be4e
Azizul Islam <111518588+azizulislam22@users.noreply.github.com>
2023-04-10 19:27:13 +0600

    Update

---
+++ A+B.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, a, b;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        scanf("%d+%d", &a, &b);
+        cout << a + b << '\n';
+    }
+
+    return 0;
+}
