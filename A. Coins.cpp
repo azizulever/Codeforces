ecabc50c779e9587df600f1ef94d8f9edfccb5b0
Azizul Islam <111518588+azizulever@users.noreply.github.com>
2023-07-03 09:54:42 +0600

    updated

---
+++ A. Coins.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define nl endl
+#define pb push_back
+#define int long long
+#define vec vector<int>
+#define N cout << "NO" << nl
+#define Y cout << "YES" << nl
+#define tc int t; cin >> t; while(t--)
+#define for0 for(int i = 0; i < n; i++)
+#define for1 for(int i = 1; i <= n; i++)
+using namespace std;
+
+int32_t main() {
+    int count = 0, n, s;
+    cin >> n >> s;
+
+    count = (s + n - 1) / n;
+    cout << count;
+
+    return 0;
+}
